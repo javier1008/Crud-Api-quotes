b65c9c2955d657fb97d30094b937a53f547aa334
javier1008 <91381224+javier1008@users.noreply.github.com>
2022-11-28 10:44:12 -0600

    first commit

---
+++ config/connectors.ts
@@ -0,0 +1,19 @@
+
+import { MongoClient, ClientMariaDB } 
+    from "../dependences.ts";
+
+
+const mongoClient = new MongoClient();
+const URI_MONGO="mongodb://root:123456789@127.0.0.1:27017/quotes?authMechanism=SCRAM-SHA-256&authSource=admin";
+export const connectorMongoDB = await mongoClient.connect(URI_MONGO);
+
+const CONN_MARIADB = {
+    hostname: "127.0.0.1",
+    port: 6034,
+    username: "devDeno",
+    password: "t0ps3cr3t",
+    db: "quotes",
+    poolSize: 10};
+
+export const connectorMariaDB = await new ClientMariaDB().connect(CONN_MARIADB);
+
